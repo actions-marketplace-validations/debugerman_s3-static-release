5ab639a8e49e839b71e600170bb549fa7825bd6b
Ning <ning@dtchain.io>
2020-06-16 11:32:29 +0800

    remove default arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -l
 set -euo pipefail
 
-SERVICE_ALIAS=${SERVICE_ALIAS}
 SERVICE_URL=${SERVICE_URL}
 MIRROR_SOURCE=${MIRROR_SOURCE:="."}
 
 # Set mc configuration
-mc config host add "$SERVICE_ALIAS" "$SERVICE_URL" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
+mc config host add "s3" "$SERVICE_URL" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
 
 # Remove all files
-mc rm --recursive --force "$SERVICE_ALIAS/$MIRROR_TARGET"
+mc rm --recursive --force "s3/$MIRROR_TARGET"
 
 # Execute mc mirror
-mc mirror $* "$MIRROR_SOURCE" "$SERVICE_ALIAS/$MIRROR_TARGET"
+mc mirror $* "$MIRROR_SOURCE" "s3/$MIRROR_TARGET"
